b41185429b84bd2f088cc3bbb4cd11b4e154d6af
Ismam Fatin Zishan <50224770+Zishan23@users.noreply.github.com>
2023-12-09 17:25:04 +1100

    Added nav bar

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BsLinkedin } from "react-icons/bs";
+
+const HeaderSocials = () => {
+  return (
+    <div className='header_socials'>
+        <a href='https://linkedin.com' targer="_blank">BsLinkedin</a>
+        <a href='https://github.com' targer="_blank"></a>
+        <a href='https://dribble.com' targer="_blank"></a>
+
+    </div>
+  )
+}
+
+export default HeaderSocials
