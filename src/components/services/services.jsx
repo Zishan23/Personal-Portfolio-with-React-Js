2494e21f307edc4dd0b64a0cd3930337571e7cf0
Ismam Fatin Zishan <50224770+Zishan23@users.noreply.github.com>
2023-12-17 04:01:22 +1100

    added services

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,9 +1,104 @@
 import React from 'react'
 import './services.css'
+import { BsCheck2 } from "react-icons/bs";
 
 const Services = () => {
   return (
-    <section id='services'>services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className="container_services">
+        <article className='service'>
+          <div className="service_head">
+            <h3>UI/UX</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className="service_head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className='service'>
+          <div className="service_head">
+            <h3>App Development</h3>
+          </div>
+
+          <ul className='service_list'>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+            <li>
+            <BsCheck2 className='service_list-icon'/>
+              <p>Lorem ipsum dolor sit amet. Cum obcaecati quia ea voluptatem harum sed</p>
+            </li>
+          </ul>
+        </article>
+
+
+
+      </div>
+    </section>
   )
 }
 
