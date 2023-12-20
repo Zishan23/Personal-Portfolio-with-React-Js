4453bfcb00b271f7e368969ea862286d1f5ba8b4
Ismam Fatin Zishan <50224770+Zishan23@users.noreply.github.com>
2023-12-20 22:50:37 +1100

    started portfolio section

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,7 +3,20 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className='portfolio_item'>
+         <div className="portfolio_item-image"></div>
+         <h3>This is a Portfolio item</h3>
+
+        </article>
+
+
+      </div>
+    </section>
   )
 }
 
