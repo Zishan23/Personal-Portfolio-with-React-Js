85b67c6cc2078d740cb177ab0d0915a5228577a9
Ismam Fatin Zishan <50224770+Zishan23@users.noreply.github.com>
2023-12-24 20:53:05 +1100

    started the reference and testimonial section

--- src/components/testimonial/testimonial.jsx
+++ src/components/testimonial/testimonial.jsx
@@ -1,10 +1,59 @@
 import React from 'react'
 import './testimonial.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
 
 const Testimonial = () => {
   return (
     <section id='testimonial'>
-      testimonial
+      <h5>References</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonial_container">
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5 className='client_name'>Ernest Achiver</h5>
+            <small className='client_review'>
+            Lorem ipsum dolor sit amet. Et autem consectetur id quas temporibus aut voluptatibus perferendis sit voluptas enim?
+            Ex molestiae sapiente qui quidem nostrum vel laudantium necessitatibu
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5 className='client_name'>Ernest Achiver</h5>
+            <small className='client_review'>
+            Lorem ipsum dolor sit amet. Et autem consectetur id quas temporibus aut voluptatibus perferendis sit voluptas enim?
+            Ex molestiae sapiente qui quidem nostrum vel laudantium necessitatibu
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5 className='client_name'>Ernest Achiver</h5>
+            <small className='client_review'>
+            Lorem ipsum dolor sit amet. Et autem consectetur id quas temporibus aut voluptatibus perferendis sit voluptas enim?
+            Ex molestiae sapiente qui quidem nostrum vel laudantium necessitatibu
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={AVTR1} alt="Avatar One" />
+            <h5 className='client_name'>Ernest Achiver</h5>
+            <small className='client_review'>
+            Lorem ipsum dolor sit amet. Et autem consectetur id quas temporibus aut voluptatibus perferendis sit voluptas enim?
+            Ex molestiae sapiente qui quidem nostrum vel laudantium necessitatibu
+            </small>
+          </div>
+        </article>
+      </div>
+
     </section>
   )
 }
