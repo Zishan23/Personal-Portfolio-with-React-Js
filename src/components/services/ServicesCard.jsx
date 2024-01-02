474d2a72ce6031ec93e5fde04d606a914130a1df
Ismam Fatin Zishan <50224770+Zishan23@users.noreply.github.com>
2024-01-03 01:44:59 +1100

    workexperience working cntinue

--- src/components/services/ServicesCard.jsx
+++ src/components/services/ServicesCard.jsx
@@ -1,50 +1,18 @@
 import React from 'react'
 import "./ServicesCard.css"
-const WORK_EXPERIENCE = [
-    {
-      title:"Senior Front End Enginner",
-      date: "April 2020 - Present",
-      responsibilities: [
-        "Lead the team",
-        "Lead the team",
-        "Lead the team",
-        "Lead the team",
-      ],
-      
-    },
-    {
-      title:"Senior Front End Enginner",
-      date: "April 2020 - Present",
-      responsibilities: [
-        "Lead the team",
-        "Lead the team",
-        "Lead the team",
-        "Lead the team",
-      ],
-    },
-    {
-      title:"Senior Front End Enginner",
-      date: "April 2020 - Present",
-      responsibilities: [
-        "Lead the team",
-        "Lead the team",
-        "Lead the team",
-        "Lead the team",
-      ],
-    }
-]  
-const ServicesCard = ( details) => {
+import { WORK_EXPERIENCE } from "./Services"
+
+const ServicesCard = ({ details }) => {
   return (
     <div className='work-experience-card'>
         <h6>{details.title}</h6>
         <div className='work-duration'>{details.date}</div>
 
         <ul>
-            {details.responsibilities.iterable.map((item) => {
-                <li key={item}>{item}</li>
-            })}
+          {details.responsibilities.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
         </ul>
-
     </div>
   )
 }
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
+import { useRef } from 'react';
 import './services.css'
 import ServicesCard from './ServicesCard';
-import { BsCheck2 } from "react-icons/bs";
+import Slider from "react-slick";
+
 
 const WORK_EXPERIENCE = [
   {
-    title:"Senior Front End Enginner",
-    date: "April 2020 - Present",
-    responsibilities: [
-      "Lead the team",
-      "Lead the team",
-      "Lead the team",
-      "Lead the team",
-    ],
-    
-  },
-  {
-    title:"Senior Front End Enginner",
+    title:"Senior Front End Engineer",
     date: "April 2020 - Present",
     responsibilities: [
       "Lead the team",
@@ -25,31 +16,60 @@ const WORK_EXPERIENCE = [
       "Lead the team",
     ],
   },
-  {
-    title:"Senior Front End Enginner",
-    date: "April 2020 - Present",
-    responsibilities: [
-      "Lead the team",
-      "Lead the team",
-      "Lead the team",
-      "Lead the team",
-    ],
-  }
   
-]
+  {
+    title:"Senior Front End Engineer",
+    date: "April 2020 - Present",
+    responsibilities: [
+      "Lead the team",
+      "Lead the team",
+      "Lead the team",
+      "Lead the team",
+    ],
+  },
+  {
+    title:"Senior Front End Engineer",
+    date: "April 2020 - Present",
+    responsibilities: [
+      "Lead the team",
+      "Lead the team",
+      "Lead the team",
+      "Lead the team",
+    ],
+  },
+];
 
 const Services = () => {
+  const sliderRef = useRef();
+  const settings = {
+    dots:false,
+    infinite:true,
+    speed:500,
+    slideToShow:2,
+    slideToScroll:1,
+    arrows:false,
+    responsive:[
+      {
+        breakpoint:700,
+        settings:{
+          slideToShow:1,
+          slideToScroll:1,
+        },
+      },
+    ],
+  }
+
   return (
     <section id='services' className='experience-container'>
       <h5>What I Offer</h5>
       <h2>Services</h2>
       <div className='experience-content'>
-        {WORK_EXPERIENCE.map((item) => {
-          <ServicesCard key={item.title} details={item}/>
-          
-        })}
+        <Slider ref={sliderRef} {...settings}>
+        {WORK_EXPERIENCE.map((item) => (
+          <ServicesCard key={item.title} details={item} />
+        ))}
+        </Slider>
       </div>
-
     </section>
   )
 }
