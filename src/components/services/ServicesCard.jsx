34a830d9fc691210a357d7c85ec0a4bc32d32a36
Ismam Fatin Zishan <50224770+Zishan23@users.noreply.github.com>
2024-01-02 23:31:58 +1100

    Added work experience

--- src/components/services/ServicesCard.jsx
+++ src/components/services/ServicesCard.jsx
@@ -1,6 +1,38 @@
 import React from 'react'
 import "./ServicesCard.css"
-
+const WORK_EXPERIENCE = [
+    {
+      title:"Senior Front End Enginner",
+      date: "April 2020 - Present",
+      responsibilities: [
+        "Lead the team",
+        "Lead the team",
+        "Lead the team",
+        "Lead the team",
+      ],
+      
+    },
+    {
+      title:"Senior Front End Enginner",
+      date: "April 2020 - Present",
+      responsibilities: [
+        "Lead the team",
+        "Lead the team",
+        "Lead the team",
+        "Lead the team",
+      ],
+    },
+    {
+      title:"Senior Front End Enginner",
+      date: "April 2020 - Present",
+      responsibilities: [
+        "Lead the team",
+        "Lead the team",
+        "Lead the team",
+        "Lead the team",
+      ],
+    }
+]  
 const ServicesCard = ( details) => {
   return (
     <div className='work-experience-card'>
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,6 +1,42 @@
 import React from 'react'
 import './services.css'
+import ServicesCard from './ServicesCard';
 import { BsCheck2 } from "react-icons/bs";
+
+const WORK_EXPERIENCE = [
+  {
+    title:"Senior Front End Enginner",
+    date: "April 2020 - Present",
+    responsibilities: [
+      "Lead the team",
+      "Lead the team",
+      "Lead the team",
+      "Lead the team",
+    ],
+    
+  },
+  {
+    title:"Senior Front End Enginner",
+    date: "April 2020 - Present",
+    responsibilities: [
+      "Lead the team",
+      "Lead the team",
+      "Lead the team",
+      "Lead the team",
+    ],
+  },
+  {
+    title:"Senior Front End Enginner",
+    date: "April 2020 - Present",
+    responsibilities: [
+      "Lead the team",
+      "Lead the team",
+      "Lead the team",
+      "Lead the team",
+    ],
+  }
+  
+]
 
 const Services = () => {
   return (
@@ -9,7 +45,7 @@ const Services = () => {
       <h2>Services</h2>
       <div className='experience-content'>
         {WORK_EXPERIENCE.map((item) => {
-          <ExperienceCard key={item.title} details={item}/>
+          <ServicesCard key={item.title} details={item}/>
           
         })}
       </div>
