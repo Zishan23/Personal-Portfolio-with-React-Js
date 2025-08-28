1a9dae1fd7f219789dfcc925d9abd8daee52229c
Ismam Fatin Zishan <50224770+Zishan23@users.noreply.github.com>
2025-08-28 22:07:07 +1000

    Fix homepage, add CNAME, correct Services import (lowercase)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/header/header'
 import Nav from './components/nav/nav'
 import About from './components/about/about'
 import Experience from './components/experience/experience'
-import Services from './components/services/Services';
+import Services from './components/services/services';
 import Portfolio from './components/portfolio/portfolio'
 import Testimonials from './components/testimonial/testimonial'
 import Contact from './components/contact/contact'
